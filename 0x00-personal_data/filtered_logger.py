002d03d9a33112a34f2b4535648fcb0450295cb9
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-04 23:53:38 +0300

    task 0. Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""0x00. Personal data"""
+
+from typing import List
+from re import sub
+
+
+def filter_datum(
+            fields: List[str],
+            redaction: str,
+            message: str,
+            separator: str
+            ) -> str:
+    """Returns the log message obfuscated"""
+    for field in fields:
+        message = sub(
+                f"{field}=.*?{separator}",
+                f"{field}={redaction}{separator}",
+                message
+            )
+    return message
