a9dd290bb2e286aeec9ce9b875131004ce2097fc
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-05 01:01:50 +0300

    1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,30 @@
 
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initialize the RedactingFormatter with a list of fields to redact
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record and redact specified fields from the message
+        """
+        return filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
 
 
 def filter_datum(
