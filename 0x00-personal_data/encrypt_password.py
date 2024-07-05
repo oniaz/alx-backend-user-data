a6c8c82bcabb2cab2bb9beb4ddc299be286ed58c
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-05 03:09:32 +0300

    task 6. Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates that the provided password matches the hashed password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
