2f2192e65d3515a6866535214db3767e9840f792
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-12 05:16:54 +0300

    task 3. User ID for Session ID  + str(generated uuid4)

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,8 +15,8 @@ class SessionAuth(Auth):
             return None
 
         session_id = uuid.uuid4()
-        self.user_id_by_session_id[session_id] = user_id
-        return session_id
+        self.user_id_by_session_id[str(session_id)] = user_id
+        return str(session_id)
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """user_id_for_session_id method docs"""
